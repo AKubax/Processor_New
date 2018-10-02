3866e8834816e9b8b75e5e39918d49661f42839d
AKubax <ovmiovmiovmi@gmail.com>
2018-10-02 22:41:42 +0300

    commit without testing

--- Compiler.h
+++ Compiler.h
@@ -11,8 +11,15 @@ int compile(std::vector<Token> inp, FILE* outFile){
 
     std::vector<char> outBuf;
 
+    std::map<const char*, size_t> labels;
+
     for(; curPtr - inp.data() < (ptrdiff_t) inp.size() ;){
         if(!curPtr[0].isStr) return printf("Invalid cmd (ñmd can't be a number)\n"), 3;
+
+        if(curPtr[0].value.str[0] == ':'){
+            labels[curPtr[0].value.str] = outBuf.size();
+            curPtr++;
+        }
 
         #define DEF_CMD(num, name, compCode, procCode) else if (strcmp(#name, curPtr[0].value.str)) { compCode; }
 
