1770b0425b4081d2cb452617ccfa09c33a0d82c7
AKubax <ovmiovmiovmi@gmail.com>
2018-11-01 00:21:22 +0300

    Macros done boiii

--- Compiler.h
+++ Compiler.h
@@ -43,7 +43,7 @@ std::map<std::string, size_t> makeLabels(std::vector<Token> inp){
 
     printd("{----------------------------------------------------------------------------\n\
     !Pre-compile                                                                                      \n\
-    }----------------------------------------------------------------------------");
+    }----------------------------------------------------------------------------\n");
 
 
     Token* curPtr = inp.data();
@@ -78,37 +78,51 @@ std::map<std::string, size_t> makeLabels(std::vector<Token> inp){
     return labels;
 }
 
-//std::vector<Token> emitMacros(const std::vector<Token> inp){
-//    std::vector<Token> retVal;
-//
-//    for(Token* ptr = inp.data(); ptr < inp.data() + inp.size(); ptr++){
-//        if( !(ptr[0].isStr) ){
-//            retVal.push_back(ptr[0]);
-//            continue;
-//        }
-//        #define DEF_MACROS(name, numArgs, code) {
-//            else if(strcmp(ptr[0].value.str, #name) == 0) {
-//                const std::vector<Token> codeTokenized = tokenize(code);
-//                for(Token* ptrInMacrosCode = codeTokenized.data(); ptrInMacrosCode < codeTokenized.data() + codeTokenized.size(); ptrInMacrosCode++){
-//                    int resAtoi = 0;
-//                    if(ptrInMacrosCode[0].value.str[0] == '$' && (resAtoi = atoi(ptrInMacrosCode[0].value.str + 1)) != 0 && (resAtoi = atoi(ptrInMacrosCode[0].value.str + 1)) <= numArgs){
-//                        retVal.push_back(ptr[resAtoi]);
-//                    }
-//                    else retVal.push_back(ptrInMacrosCode[0]);
-//                }
-//
-//                ptr += numArgs;
-//            }
-//
-//    }
-//}
+std::vector<Token> emitMacros(const std::vector<Token> inp);
+std::vector<Token> emitMacros(const std::vector<Token> inp){
+    std::vector<Token> retVal;
+
+    for(const Token* ptr = inp.data(); ptr < inp.data() + inp.size(); ptr++){
+        if( !(ptr[0].isStr) ){
+            retVal.push_back(ptr[0]);
+            continue;
+        }
+        #define DEF_MACRO(name, numArgs, code)                                                                                                                                                \
+            else if(strcmp(ptr[0].value.str, #name) == 0) {                                                                                                                                    \
+                printd("Found macro '%s'\n", #name);                                                                                                                                           \
+                                                                                                                                                                                               \
+                const std::vector<Token> codeTokenized = tokenize(code);                                                                                                                       \
+                for(const Token* ptrInMacrosCode = codeTokenized.data(); ptrInMacrosCode < codeTokenized.data() + codeTokenized.size(); ptrInMacrosCode++){                                    \
+                    printd("Token in macro code is '%s'\n", ptrInMacrosCode[0].c_str());                                                                                                       \
+                                                                                                                                                                                               \
+                    int resAtoi = 0;                                                                                                                                                           \
+                    if(ptrInMacrosCode[0].isStr && ptrInMacrosCode[0].value.str[0] == '$' && (resAtoi = atoi(ptrInMacrosCode[0].value.str + 1)) != 0 && (resAtoi = atoi(ptrInMacrosCode[0].value.str + 1)) <= numArgs){    \
+                        retVal.push_back(ptr[resAtoi]);                                                                                                                                        \
+                    }                                                                                                                                                                          \
+                    else retVal.push_back(ptrInMacrosCode[0]);                                                                                                                                 \
+                }                                                                                                                                                                              \
+                                                                                                                                                                                               \
+                ptr += numArgs;                                                                                                                                                                \
+            }
+
+        if(0);
+        #include "Macros.h"
+        #undef DEF_MACROS
+
+        else retVal.push_back(ptr[0]);
+    }
+
+    return retVal;
+}
 
 int compile(std::vector<Token> inp, FILE* outFile);
 int compile(std::vector<Token> inp, FILE* outFile){
 
     printd("{----------------------------------------------------------------------------\n\
     Compile                                                                                      \n\
-}----------------------------------------------------------------------------");
+}----------------------------------------------------------------------------\n");
+
+    inp = emitMacros(inp);
 
     printd("\n\n---\n#Compile func start\n#inp = {");
     for(auto& el : inp) el.isStr? printd(" '%s',", el.value.str) : printd(" %lf,", el.value.dbl);
